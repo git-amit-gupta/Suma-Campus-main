9ac3d04efed4a03b64a4155309959223ffa10dfe
git-amit-gupta <amit.gupta@idera.com>
2025-05-20 15:44:19 +0530

    changes reverted

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-import withReExtPlugin from '@sencha/reext/dist/ReExt/next-plugin-reext.js';
-
-const nextConfig = {
-  // Any other Next.js config goes here
-};
-
-module.exports = withReExtPlugin(nextConfig);
