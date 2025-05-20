e8a663b2fb863ef55da609b233194789514fbc69
git-amit-gupta <amit.gupta@idera.com>
2025-05-20 17:09:27 +0530

    changed firebaseConfig

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -5,14 +5,24 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore, collection, query, orderBy, limit, startAfter, getDocs ,where,getCountFromServer,getDoc,doc} from "firebase/firestore";
 
 // Replace with your Firebase config
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBltlQP8OjABHSZFL93JGWRVhGwRVle0-4",
+//     authDomain: "campus-ab9d7.firebaseapp.com",
+//     projectId: "campus-ab9d7",
+//     storageBucket: "campus-ab9d7.firebasestorage.app",
+//     messagingSenderId: "839925913336",
+//     appId: "1:839925913336:web:58b5b851aa9e595b9c4d0d"
+//   };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBltlQP8OjABHSZFL93JGWRVhGwRVle0-4",
-    authDomain: "campus-ab9d7.firebaseapp.com",
-    projectId: "campus-ab9d7",
-    storageBucket: "campus-ab9d7.firebasestorage.app",
-    messagingSenderId: "839925913336",
-    appId: "1:839925913336:web:58b5b851aa9e595b9c4d0d"
-  };
+  apiKey: "AIzaSyCBnNclmPdKY-b03Tz5-qe1MmGH9i9EXpw",
+  authDomain: "campus-7cfcb.firebaseapp.com",
+  projectId: "campus-7cfcb",
+  storageBucket: "campus-7cfcb.firebasestorage.app",
+  messagingSenderId: "772039666767",
+  appId: "1:772039666767:web:8979607274884dc7bc4e3a"
+};
+
 // Initialize Firebase (prevents multiple instances)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
