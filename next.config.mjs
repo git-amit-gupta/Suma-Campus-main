e099064efe8b371816c74053792b6d66dd07b23d
git-amit-gupta <amit.gupta@idera.com>
2025-05-20 15:24:25 +0530

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-import withReExtPlugin from './node_modules/@sencha/reext/dist/ReExt/next-plugin-reext.js';
+import {withReExtPlugin} from './node_modules/@sencha/reext/dist/ReExt/next-plugin-reext.js';
 
 const nextConfig = {};
 export default withReExtPlugin(nextConfig);
